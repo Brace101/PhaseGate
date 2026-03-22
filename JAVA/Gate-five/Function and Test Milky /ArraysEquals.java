623ca4d75ac795807410893fedc35ff2d7e93437
Adeyemi Olalekan <deyemibowen@gmail.com>
2026-03-22 21:39:18 +0100

    Gate-five

--- JAVA/Gate-five/Function and Test Milky /ArraysEquals.java
+++ JAVA/Gate-five/Function and Test Milky /ArraysEquals.java
@@ -1,14 +1,35 @@
-public class ArrayEquals {
+public class ArraysEquals {
 
-    public static int arrayEquals(int[] number1 , int []number2) {
+    public static boolean arrayEquals(int[] number1, int[] number2) {
 
-        for (int index = 0; index < number.length; index++) {
+        if (number1 == null && number2 == null) {
+            return true;
+        }
 
-            if (number1[index] == number2[index]){
-                return index;
+        if (number1 == null || number2 == null) {
+            return false;
+        }
+
+        if (number1.length != number2.length) {
+            return false;
+        }
+
+        boolean isEqual = true;
+
+        for (int index = 0; index < number1.length; index++) {
+
+            for (int count = index; count <= index; count++) {
+
+                if (number1[index] == number2[count]) {
+                    if (number1[index] != number2[count]) {
+                        isEqual = false;
+                    }
+                } else {
+                    isEqual = false;
+                }
             }
         }
-            return 
+
+        return isEqual;
     }
 }
-
--- JAVA/Gate-five/Function and Test Milky /Squares.java
+++ JAVA/Gate-five/Function and Test Milky /Squares.java
@@ -1,15 +1,12 @@
 public class Squares {
 
-    public static int[] squares(int number) {
+    public static int[] Squares(int number) {
         int[] result = new int[number];
 
         for (int index = 0; index < number; index++) {
             result[index] = (index+ 1) * (index + 1);
         }
+
         return result;
     }
 }
-
-
-
-
