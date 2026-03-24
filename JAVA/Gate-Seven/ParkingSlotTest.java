043294d573ff26b37dcbecadd44fe8f245a5b7c9
Adeyemi Olalekan <deyemibowen@gmail.com>
2026-03-24 21:38:33 +0100

    Gate-Seven

---
+++ JAVA/Gate-Seven/ParkingSlotTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ParkingSlotTest{
+
+//    @Test
+//    public void testAllSlotsEmptyOnCreation(){      
+//            
+//        int[] actual = ParkingSlot.createParkingLot(20);
+//
+//        assertEquals(20, actual.length);
+//
+//        int[] expected = new int[20];
+// 
+//        assertArrayEquals(expected, actual);  
+//    }
+//
+    @Test
+    public void testCarParkAutomatically() {
+
+        int[] parkingSlots = new int[]{0, 2, 0};
+
+        String actual = ParkingSlot.parkCarAutomatically(parkingSlots);
+
+        String expected = "Car parked at slot 1";
+
+        assertEquals(expected, actual);
+
+        assertArrayEquals(new int[]{1, 2, 0}, parkingSlots);
+    }
+}
+
